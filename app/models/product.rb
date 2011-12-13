230bd91f119a80ec5563b2b6a369e2720cd3b904
David Stearns <dstearns@stearnsusa.com>
2011-12-13 08:26:58 -0800

    Add Store Front

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,12 @@
 class Product < ActiveRecord::Base
 
+default_scope :order => 'title'
+
+
 validates :title, :description, :image_url, :presence => true
 validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
 validates :title, :uniqueness => true
-validates :image_url, :format => {:with => %r{\.(gif|jpg|png)$}, :message => 'must be a URL for GIF, JPG or PNG image.' 
-                                 }
+validates :image_url, :format => {:with => %r{\.(gif|jpg|png)$}, :message => 'must be a URL for GIF, JPG or PNG image.' }
+validates :title, :length => {:minimum => 10, :message => "This title is too short : minimum length is 10"}
 
 end
