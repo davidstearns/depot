971c3ddc6deaf0c83b4c0aa22fde790292442539
David Stearns <dstearns@stearnsusa.com>
2011-12-13 10:05:57 -0800

    add date & time

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,6 +2,8 @@ class StoreController < ApplicationController
   def index
 
     @products = Product.all
+    @time = Time.now.strftime("%H:%M:%S")
+    @date = Time.now.strftime("%m/%d/%Y")
 
   end
 
